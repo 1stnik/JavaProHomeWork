091e1dbe473645a8d60d4b39a78a58af473b0f08
Serhii Nikonenko <1stnik@gmail.com>
2022-10-17 23:01:46 +0300

    add AverageSum and ignore file

---
+++ HomeWork2/src/average_sum/AverageSum.java
@@ -0,0 +1,15 @@
+package average_sum;
+
+public class AverageSum {
+
+    public static void main(String[] args) {
+
+        int x = 3;
+        int y = 10;
+        double sum;
+
+        sum = (double) (x + y) / 2;
+        System.out.println(sum);
+
+    }
+}
