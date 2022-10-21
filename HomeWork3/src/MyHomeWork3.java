ec3d3728aa0cfce105213c7e9dcf22436d7746f1
Serhii Nikonenko <1stnik@gmail.com>
2022-10-21 16:26:07 +0300

    add task 8 -10

--- HomeWork3/src/MyHomeWork3.java
+++ HomeWork3/src/MyHomeWork3.java
@@ -71,7 +71,7 @@ public class MyHomeWork3 {
             num -= 5;
         }
 
-        // 7. Необходимо вывести на экран таблицу умножения на Х: (любое число вводимое из консоли)
+//         7. Необходимо вывести на экран таблицу умножения на Х: (любое число вводимое из консоли)
         System.out.println("\n-----------------------------------\nTask 7:");
         Scanner scnr = new Scanner(System.in);
         System.out.println("Enter any number");
@@ -86,6 +86,48 @@ public class MyHomeWork3 {
         // 8. Создайте массив, содержащий 10 первых нечетных чисел.
         //    Выведете элементы массива на консоль в одну строку, разделяя запятой.
         System.out.println("-----------------------------------\nTask 8:");
+        int[] oddDigits = new int[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+        for (int o : oddDigits) System.out.print(o + ", ");
 
-    }
+        System.out.println("\n...and one more version:");
+        int[] arrInt = new int[10];
+        int counter = 1;
+        int arrCounter = 0;
+        while (arrCounter < arrInt.length) {
+            if (counter % 2 != 0) {
+                arrInt[arrCounter] = counter;
+                System.out.print(arrInt[arrCounter] + ", ");
+                arrCounter++;
+            }
+            counter++;
+        }
+
+        // 9. Дан массив размерности N, найти наименьший элемент массива и вывести на консоль
+        //    (если наименьших элементов несколько — вывести их все).
+        System.out.println("\n-----------------------------------\nTask 9:");
+        int[] arrayForTask = new int[]{3, 2, -4, 5, 9, -1, -4, 6, 8, 2, 9, -4, 1};
+        int minValue = arrayForTask[0];
+        System.out.print("Min value: ");
+        for (int a = 1; a < arrayForTask.length; a++) {
+            if (minValue > arrayForTask[a]) {
+                minValue = arrayForTask[a];
+            } else if (minValue == arrayForTask[a]) System.out.print(minValue + " ");
+        }
+        System.out.println(minValue + " ");
+
+        // 10. В массиве из задания 9. найти наибольший элемент.
+        System.out.println("-----------------------------------\nTask 10:");
+        int maxValue = arrayForTask[0];
+        System.out.print("Max value: ");
+        for (int a = 1; a < arrayForTask.length; a++) {
+            if (maxValue < arrayForTask[a]) {
+                maxValue = arrayForTask[a];
+            } else if (maxValue == arrayForTask[a]) System.out.print(maxValue + " ");
+        }
+        System.out.print(maxValue + " ");
+
+
+
+
+}
 }
